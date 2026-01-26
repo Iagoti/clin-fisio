471a8479b5f5587eeb9e79229193172420876f5c
Iagoti <iagoti03@gmail.com>
2026-01-26 00:30:51 -0300

    feature: init project

---
+++ src/main/java/com/system/fisio/ClinFisioApplication.java
@@ -0,0 +1,13 @@
+package com.system.fisio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinFisioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinFisioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/fisio/ClinFisioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.fisio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinFisioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
